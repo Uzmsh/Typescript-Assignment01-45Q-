577ad58d0babb5d73619a38827697dabdc0ffa6e
Uzma Shahzad <93823334+Uzmsh@users.noreply.github.com>
2024-03-28 20:41:34 +0500

    Update main.ts

--- 42_gmagician/main.ts
+++ 42_gmagician/main.ts
@@ -1,7 +1,7 @@
 
 
 // Array of magician's names
-let magicians: string[] = ["Harry Houdini", "David Copperfield", "Penn Jillette", "Teller"];
+let magicians: string[] = ["Haris", "Dawood", "Junaid", "Tallal"];
 
 function show_magicians(magicians: string[]): void {
     for (let magician of magicians) {
