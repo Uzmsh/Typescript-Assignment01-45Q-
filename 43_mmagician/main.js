c5dd99c9df6aff67725b1c96c7c7aed53259a8c2
Uzma Shahzad <93823334+Uzmsh@users.noreply.github.com>
2024-03-28 20:41:06 +0500

    Update main.js

--- 43_mmagician/main.js
+++ 43_mmagician/main.js
@@ -8,7 +8,7 @@ var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
     return to.concat(ar || Array.prototype.slice.call(from));
 };
 // Original array of magician's names
-var magicians = ["Harry Houdini", "David Copperfield", "Penn Jillette", "Teller"];
+var magicians = ["Haris", "Dawood", "Junaid", "Tallal"];
 function show_magicians(magicians) {
     for (var _i = 0, magicians_1 = magicians; _i < magicians_1.length; _i++) {
         var magician = magicians_1[_i];
